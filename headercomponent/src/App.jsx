cc79e66c0c1cd8e3fe640b5b7e79dfaf634daa42
tanmaykulkarni2112 <149245022+tanmaykulkarni2112@users.noreply.github.com>
2024-08-03 17:04:55 +0530

    Update App.jsx

--- headercomponent/src/App.jsx
+++ headercomponent/src/App.jsx
@@ -1,5 +1,6 @@
 //Create a react app that has a  Header component that takes a title as a prop and renders it inside a div The top level App component renders 2 Headers
 
+// import React, {Fragment} from "react"
 
 //App componenet
 function App(){
@@ -10,6 +11,17 @@ function App(){
         </div>
     )
 }
+
+//we can also do this using fragment
+// function App(){
+//     return(
+//         <Fragment>
+//             <Header title = "tanmay"></Header>
+//             <Header title = "Arnav"></Header>
+//         </Fragment>
+//     )
+// }
+
 
 // Header component
 // Object destructuring 
